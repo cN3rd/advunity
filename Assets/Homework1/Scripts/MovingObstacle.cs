a9b0a25e48c96547e15c0721715eac6edffb63cf
cN3rd <subs.in.tokyo@gmail.com>
2024-12-21 23:59:28 +0200

    Adjust moving obstacles

--- Assets/Homework1/Scripts/MovingObstacle.cs
+++ Assets/Homework1/Scripts/MovingObstacle.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Homework1
@@ -7,23 +8,56 @@ namespace Homework1
         [SerializeField] private Transform location1;
         [SerializeField] private Transform location2;
         [SerializeField] private float speed = 5f;
-
+        [SerializeField, HideInInspector] private Bounds obstacleBounds;
+        
+        private Vector3 _currentTargetPosition;
         private Transform _targetLocation;
 
-        private void Start() =>
+        private void Start()
+        {
             _targetLocation = location1;
+            CalculateTargetPosition();
+        }
 
         private void Update()
         {
-            // Move the NavMeshObstacle towards the target location
-            transform.position = Vector3.MoveTowards(transform.position, _targetLocation.position,
-                speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                _currentTargetPosition,
+                speed * Time.deltaTime
+            );
 
-            if (Vector3.Distance(transform.position, _targetLocation.position) < 0.1f)
-            {
-                // Switch target location when the obstacle reaches the current target
-                _targetLocation = _targetLocation == location1 ? location2 : location1;
-            }
+            if (!(Vector3.Distance(transform.position, _currentTargetPosition) < 0.01f)) return;
+
+            _targetLocation = _targetLocation == location1 ? location2 : location1;
+            CalculateTargetPosition();
         }
+
+        private void CalculateTargetPosition()
+        {
+            int direction = _targetLocation == location1 ? 1 : -1;
+            Vector3 bodyOffset = direction * new Vector3(0, 0, obstacleBounds.size.z / 2);
+            _currentTargetPosition = _targetLocation.position - bodyOffset;
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate() => obstacleBounds = CalculateBounds();
+
+        [UsedImplicitly]
+        private Bounds CalculateBounds()
+        {
+            var renderers = GetComponentsInChildren<Renderer>(true);
+            if (renderers.Length == 0)
+                return new Bounds(transform.position, Vector3.zero);
+
+            Bounds combinedBounds = renderers[0].bounds;
+            for (int i = 1; i < renderers.Length; i++)
+            {
+                combinedBounds.Encapsulate(renderers[i].bounds);
+            }
+
+            return combinedBounds;
+        }
+#endif
     }
 }
