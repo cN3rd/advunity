f681bcdfc449a9f15c21fcda23b406053763825d
Blu3Hawks <ylb.hen.sayag@gmail.com>
2025-01-05 12:05:53 +0200

    Events Working - Hen's stuff
    that works now, you can move the objects to make them collide and then trigger the events. You can see it on the inspector

--- Assets/Homework2/Scripts/Ob2.cs
+++ Assets/Homework2/Scripts/Ob2.cs
@@ -9,21 +9,47 @@ namespace Homework2.Scripts
         public event UnityAction<int> SayInteger;
         public event UnityAction<string> SayString;
 
-        [SerializeField] private UnityEvent HitShlomo;
-        [SerializeField] private UnityEvent<GameObject> HitMark;
+        [SerializeField] private UnityEvent HitShlomo = new UnityEvent();
+        [SerializeField] private UnityEvent<GameObject> HitMark = new UnityEvent<GameObject>();
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!other.CompareTag("OB1")) return;
-            
+            if (!other.CompareTag("OB1"))
+                return;
+
             Ob1 ob1 = other.GetComponent<Ob1>();
             SayHi += ob1.OnHitHi;
             SayInteger += ob1.OnHitInt;
             SayString += ob1.OnHitString;
-            
+
             SayHi?.Invoke();
             SayInteger?.Invoke(ob1.GetDamage());
             SayString?.Invoke(ob1.GetString());
+
+            HitShlomo?.Invoke();
+            HitMark?.Invoke(ob1.gameObject);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag("OB1")) return;
+
+            Ob1 ob1 = other.GetComponent<Ob1>();
+
+            SayHi -= ob1.OnHitHi;
+            SayInteger -= ob1.OnHitInt;
+            SayString -= ob1.OnHitString;
+        }
+
+        public void OnHitShlomo()
+        {
+            Debug.Log("Hi Shlomo, we kinda hit you here");
+        }
+
+        public void OnHitMark(GameObject gameObject)
+        {
+            Debug.Log("Hey it's Mark, sorry for hitting you");
+            Debug.Log(gameObject.name);
         }
     }
 }
