9599b615938a1e3612c0e928310f2f662dc00ef7
Blu3Hawks <ylb.hen.sayag@gmail.com>
2025-01-05 12:05:53 +0200

    Events Working - Hen's stuff
    that works now, you can move the objects to make them collide and then trigger the events. You can see it on the inspector

--- Assets/Homework2/Scripts/PrefabManager.cs
+++ Assets/Homework2/Scripts/PrefabManager.cs
@@ -6,14 +6,6 @@ namespace Homework2.Scripts
 {
     public class PrefabManager : MonoBehaviour
     {
-        public event UnityAction SayHi;
-        public event UnityAction<int> SayInteger;
-        public event UnityAction<string> SayString;
-        
-        [SerializeField] private Ob1 ob1;
-        [SerializeField] private Ob2 ob2;
 
-        [SerializeField] private UnityEvent HitShlomo;
-        [SerializeField] private UnityEvent<GameObject> HitMark;
     }
 }
