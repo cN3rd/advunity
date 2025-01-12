40952c2617947f0f290beca5834a14944e13ea70
cN3rd <subs.in.tokyo@gmail.com>
2025-01-12 18:56:18 +0200

    More balancing

--- Assets/Homework2/Scripts/GameManager.cs
+++ Assets/Homework2/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,9 +6,11 @@ namespace Homework2.Scripts
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private Transform shootTransform;
         [SerializeField] private PrefabCollection prefabCollection;
         [SerializeField] private Enemy enemy;
         [SerializeField] private TextMeshProUGUI healthText;
+        [SerializeField] private int shootSpeed = 50;
 
         private void Start() => healthText.text = $"Mark's HP:\n{enemy.Health:N0}";
 
@@ -37,14 +38,16 @@ namespace Homework2.Scripts
 
         private void SpawnBullet(GameObject prefab)
         {
-            // TODO: proper shooting from place
-            GameObject instance = Instantiate(prefab, transform.position + new Vector3(0, 10, 0),
-                Quaternion.identity);
-
+            GameObject instance = Instantiate(prefab, shootTransform.position, Quaternion.identity);
+            Destroy(instance, 5);
+            
             if (!instance.TryGetComponent(out Bullet bullet)) return;
+            if (!instance.TryGetComponent(out Rigidbody instanceRigidbody)) return;
 
             bullet.OnHitEnemy += OnHitEnemy;
             bullet.OnBulletCollision += OnBulletCollision;
+
+            instanceRigidbody.AddForce(Vector3.forward * shootSpeed, ForceMode.Impulse);
         }
 
         private void OnBulletCollision(BulletCollisionEventArgs args)
@@ -60,5 +63,10 @@ namespace Homework2.Scripts
             $"Mark got hit by {enemyHitInfo.Damage} points.\nRemaining Mark's HP:\n{enemyHitInfo.RemainingHealth:N0}";
 
         public void UpdateStatusOnDeath() => healthText.text = "Mark's dead, long live Eldar.";
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(shootTransform.position, shootTransform.position + Vector3.forward * 100);
+        }
     }
 }
