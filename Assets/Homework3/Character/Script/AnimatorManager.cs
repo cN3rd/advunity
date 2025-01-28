c0977ab4cd0cfbd6cd9277dc3535b359439c7cae
Madar215 <galmadar2424@gmail.com>
2025-01-28 11:03:52 +0200

    Animation Controller & Model

---
+++ Assets/Homework3/Character/Script/AnimatorManager.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Homework3.Character.Script
+{
+    public class AnimatorManager : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+
+        private static readonly int IdleHashed = Animator.StringToHash("idleSpeed");
+        private static readonly int MoveHashed = Animator.StringToHash("moveSpeed");
+
+        private void Start()
+        {
+            animator.SetFloat(IdleHashed, 1.5f);
+            animator.SetFloat(MoveHashed, 1.2f);
+        }
+    }
+}
